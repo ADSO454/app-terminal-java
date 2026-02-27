a4528e18328acee23bac54ff78bc7375303dd5ce
Jeyson David Zuñiga Gomez <jdzunigag@sena.edu.co>
2026-02-27 17:21:38 -0500

    adding new user method
    Se añade el metodo para crear nuevos usuarios

--- src/main/java/co/edu/sena/pruebabasedatos_sql_local/PruebaBaseDatos_SQL_Local.java
+++ src/main/java/co/edu/sena/pruebabasedatos_sql_local/PruebaBaseDatos_SQL_Local.java
@@ -57,4 +57,8 @@ public class PruebaBaseDatos_SQL_Local {
             System.out.println("Hubo un error al acceder a la base de datos: " + e.getMessage());
         }
     }
+    
+    public static void anadirUsuarios() {
+    System.out.println("Añadiendo usuarios...");
+}
 }
