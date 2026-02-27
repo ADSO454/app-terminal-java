5a59a10e3f1fa0bca435a3ebbeee4f600a70ea9a
JeyZG <jdzg1124@gmail.com>
2026-02-27 17:38:25 -0500

    add see users method
    Se añade el metodo para ver usuarios

--- src/main/java/co/edu/sena/pruebabasedatos_sql_local/PruebaBaseDatos_SQL_Local.java
+++ src/main/java/co/edu/sena/pruebabasedatos_sql_local/PruebaBaseDatos_SQL_Local.java
@@ -5,6 +5,7 @@ import java.sql.*;
 public class PruebaBaseDatos_SQL_Local {
 
     public static void main(String[] args) {
+		
         try {
             int documento;
             String nombres, apellidos, ciudad;
